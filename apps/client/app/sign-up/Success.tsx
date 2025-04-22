6ae8f809b6e75d296e1df4e1217f01574844108d
henry.chou <HENRY.CHOU@deltaww.com>
2025-04-23 05:28:19 +0800

    feat: successful page style change

--- apps/client/app/sign-up/Success.tsx
+++ apps/client/app/sign-up/Success.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 import Button from '../../components/Button';
 import Title from '../../components/Title';
-import Link from 'next/link';
+import { CheckIcon } from '@radix-ui/react-icons';
 
 const Success = () => {
 
   return (
     <div className="w-full py-16 px-12 flex flex-col h-full flex-1">
-      <Title className="mb-40 text-left w-33 pr-28 font-bold text-primary">
+      <Title className="mb-40 text-left w-33 font-bold text-primary">
         Welcome to the Club
       </Title>
-      <h4 className="text-primary text-xl">Account activated</h4>
+      <h4 className="text-primary text-xl mx-auto flex">
+        <div className="mr-2 rounded-3xl bg-green-900 text-white mt-0.5">
+          <CheckIcon className="mr-2 w-full" height={24} />
+        </div>
+        Account activated
+      </h4>
       <div className="mt-auto">
-        <Link href="/garage/add">
-          <Button>Register My Products Now</Button>
-        </Link>
-        <Link href="/home">
-          <Button theme="transparent" className="text-white">
-            Skip
-          </Button>
+        <Link href="/">
+          <Button>Home</Button>
         </Link>
       </div>
     </div>
