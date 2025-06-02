7b019690f9b2f6c82ebcb7055e12798cbf012e49
henry.chou <HENRY.CHOU@deltaww.com>
2025-06-02 19:50:06 +0800

    feat: update the product list for signing up

--- apps/client/utils/index.ts
+++ apps/client/utils/index.ts
@@ -1,7 +1,27 @@
 export const NOOP = () => {}
 export const API_PUBLIC_HOST = process.env.NEXT_PUBLIC_API_URL?.split('/api')[0];
 export const DEFAULT_MODELS = [
-  { id: 1, title: 'Ninja 400' },
-  { id: 2, title: 'Duke 250' },
-  { id: 3, title: 'Duke 390' },
+  { id: 1, title: 'QG125' },
+  { id: 2, title: 'CG125-I' },
+  { id: 3, title: 'Sprint 150' },
+  { id: 4, title: 'D-Super 150' },
+  { id: 5, title: 'Tiger 150' },
+  { id: 6, title: 'Tiger 200' },
+  { id: 7, title: 'Champion 200' },
+  { id: 8, title: 'RKT 200' },
+  { id: 9, title: 'QGL-G8' },
+  { id: 10, title: 'QGL-G8 Alloy' },
+  { id: 11, title: 'QGL-G12' },
+  { id: 12, title: 'QGL-G13' },
+  { id: 13, title: 'Cruiser 125' },
+  { id: 14, title: 'Monster 150' },
+  { id: 15, title: 'Wisdom 110-66' },
+  { id: 16, title: 'GD 110' },
+  { id: 17, title: 'Reiz 110' },
+  { id: 18, title: 'Velocity 110' },
+  { id: 19, title: 'Target 110' },
+  { id: 20, title: 'Agro 150' },
+  { id: 21, title: 'Agro 200' },
 ];
+
+
