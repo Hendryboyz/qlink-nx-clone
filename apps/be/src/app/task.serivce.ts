b807730427ae9ae15828e58c5a0033e174e353b0
Hendryboyz <hendry.js1247@gmail.com>
2025-08-06 17:05:21 +0800

    feat: verify product 2 times with salesforce at 9 am/pm

--- apps/be/src/app/task.serivce.ts
+++ apps/be/src/app/task.serivce.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { ProductService } from '$/modules/product/product.service';
 
 @Injectable()
@@ -7,7 +7,10 @@ export class TaskService {
   private readonly logger = new Logger(this.constructor.name);
   constructor(private readonly productService: ProductService) {
   }
-  @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron("0 9/12 * * *", {
+    name: "job verify product with salesforce",
+    timeZone: 'Asia/Taipei',
+  })
   async autoVerifyProductJob() {
     this.logger.debug('start to verify product');
     await this.productService.autoVerifyProducts();
