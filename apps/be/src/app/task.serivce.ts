a6c4376857a9b9d16df5a8dd1104c95572373e3f
Hendryboyz <hendry.js1247@gmail.com>
2025-10-15 20:20:18 +0800

    feat: schedule to resync member/products

--- apps/be/src/app/task.serivce.ts
+++ apps/be/src/app/task.serivce.ts
@@ -1,18 +1,38 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { ProductService } from '$/modules/product/product.service';
+import { UserService } from '$/modules/user/user.service';
 
 @Injectable()
 export class TaskService {
   private readonly logger = new Logger(this.constructor.name);
-  constructor(private readonly productService: ProductService) {
-  }
+  constructor(
+    private readonly productService: ProductService,
+    private readonly userService: UserService,
+  ) {}
+
   @Cron("0 9/12 * * *", {
-    name: "job verify product with salesforce",
+    name: "job to verify product with salesforce",
     timeZone: 'Asia/Taipei',
   })
   async autoVerifyProductJob() {
     this.logger.debug('start to verify product');
     await this.productService.verifyAllProducts();
+  }
+
+  @Cron("* * * * *", {
+    name: 'job to re-sync products to salesforce',
+    timeZone: 'Asia/Taipei',
+  })
+  async resyncProducts() {
+    await this.productService.reSyncCRM()
+  }
+
+  @Cron("0 2/6 * * *", {
+    name: 'job to re-sync members to salesforce',
+    timeZone: 'Asia/Taipei',
+  })
+  async resyncMembers() {
+    await this.userService.reSyncCRM();
   }
 }
