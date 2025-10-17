b47692cf3b5da7cd2c5ccef3b082fbcf2d0189ca
Hendryboyz <hendry.js1247@gmail.com>
2025-10-17 14:46:15 +0800

    refactor: apply bo module to organize bo route

--- apps/be/src/app.module.ts
+++ apps/be/src/app.module.ts
@@ -4,19 +4,14 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './database.module';
 import { ProductModule } from './modules/product/product.module';
-import { BoAuthModule } from './modules/bo/auth/auth.module';
-import { RouterModule } from '@nestjs/core';
 import { PostsModule } from './modules/posts/posts.module';
-import { PostsBoModule } from './modules/bo/posts/posts.module';
 import { StorageModule } from '$/modules/upload/storage.module';
-import { BoUserModule } from '$/modules/bo/user/bo-user.module';
 import { NotificationModule } from './notification/notification.module';
 import { VerificationModule } from './modules/bo/verification/verification.module';
 import { CrmModule } from './modules/crm/crm.module';
 import { TaskService } from '$/app/task.serivce';
 import { ScheduleModule } from '@nestjs/schedule';
-import { VehiclesModule } from './modules/bo/vehicles/vehicles.module';
-import { StatisticModule } from './modules/bo/statistic/statistic.module';
+import { BoModule } from '$/modules/bo/bo.module';
 
 @Module({
   imports: [
@@ -25,46 +20,15 @@ import { StatisticModule } from './modules/bo/statistic/statistic.module';
       isGlobal: true,
     }),
     DatabaseModule,
-    UserModule,
-    AuthModule,
-    ProductModule,
-    PostsModule,
-    BoAuthModule,
-    PostsBoModule,
-    BoUserModule,
-    VehiclesModule,
-    StorageModule,
-    StatisticModule,
-    RouterModule.register([
-      {
-        path: 'bo',
-        children: [
-          {
-            path: 'posts',
-            module: PostsBoModule,
-          },
-          {
-            path: 'auth',
-            module: BoAuthModule,
-          },
-          {
-            path: 'users',
-            module: BoUserModule,
-          },
-          {
-            path: 'vehicles',
-            module: VehiclesModule,
-          },
-          {
-            path: 'statistic',
-            module: StatisticModule,
-          },
-        ],
-      },
-    ]),
     NotificationModule,
     VerificationModule,
     CrmModule,
+    UserModule,
+    AuthModule,
+    ProductModule,
+    StorageModule,
+    PostsModule,
+    BoModule,
   ],
   providers: [TaskService],
 })
