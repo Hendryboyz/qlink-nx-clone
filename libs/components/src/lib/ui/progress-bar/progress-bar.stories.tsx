6268b49a1422c150cb35b3e6e46d30e7395523cd
KenFront <flamma0917@gmail.com>
2025-11-20 20:19:32 +0800

    Add progress bar component

---
+++ libs/components/src/lib/ui/progress-bar/progress-bar.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ProgressBar from './progress-bar';
+
+const meta: Meta<typeof ProgressBar> = {
+  title: 'Shadcn/ProgressBar',
+  component: ProgressBar,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof ProgressBar>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div className='w-[350px]'>
+        <ProgressBar list={['1', '2', '3']} />
+      </div>
+    );
+  },
+};
+
+export const Default1: Story = {
+  render: () => {
+    return (
+      <div className='w-[350px]'>
+        <ProgressBar list={['1', '2', '3']} runningIndex={1} />
+      </div>
+    );
+  },
+};
+
+export const Default2: Story = {
+  render: () => {
+    return (
+      <div className='w-[350px]'>
+        <ProgressBar list={['1', '2', '3']} runningIndex={2} />
+      </div>
+    );
+  },
+};
---
+++ libs/components/src/lib/ui/progress-bar/progress-bar.tsx
@@ -0,0 +1,83 @@
+import { Fragment, type FC } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+enum CircleStatus {
+  Running = 'running',
+  Finished = 'finished',
+  Waiting = 'waiting',
+}
+
+const commonClassnames =
+  'size-6 rounded-full text-base font-manrope-bold grid grid-cols-1 place-items-center flex-none';
+
+const Circle = ({
+  status,
+  number,
+}: {
+  status: CircleStatus;
+  number: string;
+}) => {
+  switch (status) {
+    case CircleStatus.Running:
+      return (
+        <div className={twMerge(commonClassnames, 'bg-primary text-white')}>
+          {number}
+        </div>
+      );
+    case CircleStatus.Waiting:
+      return (
+        <div
+          className={twMerge(
+            commonClassnames,
+            'bg-white text-primary border border-primary'
+          )}
+        >
+          {number}
+        </div>
+      );
+    case CircleStatus.Finished:
+      return (
+        <div className={twMerge(commonClassnames, 'bg-stroke-w text-white')}>
+          {number}
+        </div>
+      );
+    default:
+      return null;
+  }
+};
+
+type ProgressBarProps = {
+  list: string[];
+  runningIndex?: number;
+};
+
+const ProgressBar: FC<ProgressBarProps> = ({ list, runningIndex = 0 }) => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      {list.map((item, index) => (
+        <Fragment key={item}>
+          <Circle
+            status={
+              index < runningIndex
+                ? CircleStatus.Finished
+                : index === runningIndex
+                ? CircleStatus.Running
+                : CircleStatus.Waiting
+            }
+            number={item}
+          />
+          {index !== list.length - 1 && (
+            <div
+              className={twMerge(
+                'flex-auto h-[1px]',
+                index < runningIndex ? 'bg-stroke-w' : 'bg-primary'
+              )}
+            />
+          )}
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default ProgressBar;
