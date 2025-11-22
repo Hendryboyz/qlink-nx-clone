8bb133bdaa4e597fc1f7f6d6afa46e6ca13549ff
KenFront <flamma0917@gmail.com>
2025-11-22 20:55:13 +0800

    Add top nav

---
+++ libs/components/src/lib/ui/top-nav/top-nav.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TopNav from './top-nav';
+
+const meta: Meta<typeof TopNav> = {
+  title: 'Shadcn/TopNav',
+  component: TopNav,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof TopNav>;
+
+export const Default: Story = {
+  render: () => {
+    return <TopNav />;
+  },
+};
+
+export const SignedIn: Story = {
+  render: () => {
+    return <TopNav isSignedIn={true} />;
+  },
+};
